6ce66f7873d57ae883d927b88a9bd49056672c52
peterc-s <pjcsmail@gmail.com>
2024-06-11 16:01:27 +0100

    validation nearly done

--- src/base/constants/mod.rs
+++ src/base/constants/mod.rs
@@ -1,5 +1,8 @@
-pub mod constants {
-    use vulkanalia::Version;
+use vulkanalia::{
+    Version,
+    vk::ExtensionName,
+};
 
-    pub const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
-}
+pub const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
+pub const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
+pub const VALIDATION_LAYER: ExtensionName = ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
--- src/base/mod.rs
+++ src/base/mod.rs
@@ -1,9 +1,26 @@
-mod constants;
+pub mod constants;
+
+pub mod app_data {
+    use vulkanalia::vk::DebugUtilsMessengerEXT;
+
+    #[derive(Clone, Debug)]
+    pub struct DebugMessenger {
+        pub messenger: DebugUtilsMessengerEXT,
+    }
+}
 
 pub mod app {
+    /*
+     * imports
+     */
+
+    use super::constants::*;
+    use super::app_data::*;
+
     use anyhow::{anyhow, Result};
     use log::*;
     
+    use vulkanalia::vk::ExtDebugUtilsExtension;
     use winit::window::Window;
 
     use vulkanalia::{
@@ -12,14 +29,21 @@ pub mod app {
         prelude::v1_0::*,
     };
 
-    use self::constants::PORTABILITY_MACOS_VERSION;
+    use std::{
+        collections::HashSet,
+        ffi::CStr,
+        os::raw::c_void,
+    };
 
-    use super::constants::*;
-    
+    /*
+     * the vulkan app
+     */
+
     #[derive(Clone, Debug)]
     pub struct App {
         pub entry: Entry,
         pub instance: Instance,
+        pub debug_messenger: Option<DebugMessenger>,
     }
 
     impl App {
@@ -29,10 +53,17 @@ pub mod app {
             let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
             let instance = create_instance(window, &entry)?;
 
+            // create the debug messenger for the validation layer
+            let debug_messenger = if VALIDATION_ENABLED {
+                Some(create_debug_messenger(&instance)?)
+            } else {
+                None
+            };
 
             Ok(Self {
                 entry,
                 instance,
+                debug_messenger,
             })
         }
 
@@ -41,11 +72,22 @@ pub mod app {
         }
 
         pub unsafe fn destroy(&mut self) {
+            // destroy the debug messener if it exists
+            match &self.debug_messenger {
+                Some(debug_messenger) => self.instance.destroy_debug_utils_messenger_ext(debug_messenger.messenger, None),
+                _ => {}
+            };
+
             self.instance.destroy_instance(None);
         }
     }
 
+    /*
+     * creation functions
+     */
+
     unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+        // create application info struct
         let application_info = vk::ApplicationInfo::builder()
             .application_name(b"Vulkan Testing\0")
             .application_version(vk::make_version(1, 0, 0))
@@ -53,11 +95,32 @@ pub mod app {
             .engine_version(vk::make_version(1, 0, 0))
             .api_version(vk::make_version(1, 0, 0));
 
+        // get available layer names in a hashset
+        let available_layers = entry
+            .enumerate_instance_layer_properties()?
+            .iter()
+            .map(|l| l.layer_name)
+            .collect::<HashSet<_>>();
+
+        // check if validation enabled and supported
+        if VALIDATION_ENABLED && !available_layers.contains(&VALIDATION_LAYER) {
+            return Err(anyhow!("Validation layer requested but not supported."));
+        }
+
+        // add validation layer if enabled
+        let layers = if VALIDATION_ENABLED {
+            vec![VALIDATION_LAYER.as_ptr()]
+        } else {
+            Vec::new()
+        };
+
+        // get required extensions
         let mut extensions = vk_window::get_required_instance_extensions(window)
             .iter()
             .map(|e| e.as_ptr())
             .collect::<Vec<_>>();
 
+        // get flags if target is macos
         let flags = if cfg!(target_os = "macos") &&
             entry.version()? >= PORTABILITY_MACOS_VERSION {
             info!("Enabling extensions for MacOS portability.");
@@ -70,9 +133,48 @@ pub mod app {
 
         let info = vk::InstanceCreateInfo::builder()
             .application_info(&application_info)
+            .enabled_layer_names(&layers)
             .enabled_extension_names(&extensions)
             .flags(flags);
 
         Ok(entry.create_instance(&info, None)?)
+    }
+
+    unsafe fn create_debug_messenger(instance: &Instance) -> Result<DebugMessenger> {
+        assert!(VALIDATION_ENABLED);
+
+        let debug_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+            .message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::all())
+            .message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
+            .user_callback(Some(debug_callback));
+
+        let messenger = instance.create_debug_utils_messenger_ext(&debug_info, None)?;
+
+        Ok(DebugMessenger {
+            messenger,
+        })
+    }
+
+    // debug callback for validation layer
+    extern "system" fn debug_callback(
+        severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+        type_: vk::DebugUtilsMessageTypeFlagsEXT,
+        data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+        _: *mut c_void,
+    ) -> vk::Bool32 {
+        let data = unsafe { *data };
+        let message = unsafe { CStr::from_ptr(data.message) }.to_string_lossy();
+
+        if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::ERROR {
+            error!("({:?}) {}", type_, message);
+        } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::WARNING {
+            warn!("({:?}) {}", type_, message);
+        } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::INFO {
+            debug!("({:?}) {}", type_, message);
+        } else {
+            trace!("({:?}) {}", type_, message);
+        }
+
+        vk::FALSE
     }
 }
